17d8ac0609be6fd1b869a728a04334b9df94b3a7
cssrumi <33814533+cssrumi@users.noreply.github.com>
2019-02-01 08:53:13 +0000

    Delete run-zookeeper-and-kafka.bat

--- run-zookeeper-and-kafka.bat
+++
@@ -1,6 +0,0 @@
-@ECHO OFF
-cd D:\Kafka\kafka_2.11-2.0.0\bin\windows\
-start "" zookeeper-server-start.bat "..\..\config\zookeeper.properties"
-timeout /t 7 /nobreak
-start "" kafka-server-start.bat "..\..\config\server.properties"
-exit 0
